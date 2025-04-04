97d9c37f1568e97e48d4e11dff61a938a8c68f6d
Akkuro <157906714+Akkuro@users.noreply.github.com>
2025-04-04 10:44:50 +0200

    Add empty cart message conditionnally

--- fake-store-app/src/app/cart/page.tsx
+++ fake-store-app/src/app/cart/page.tsx
@@ -1,10 +1,20 @@
-export default function Cart() {
+import { Product } from "@/interfaces/Product";
+import React from "react";
+
+interface Props {
+  products: Product[];
+}
+
+const Cart: React.FC<Props> = ({ products }) => {
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold mb-4">Votre Panier</h1>
+      <h1 className="text-3xl font-bold mb-4">Your cart</h1>
       <p className="text-lg">
-        Vous n'avez encore ajouté aucun produit à votre panier.
+        {(products == null || products.length === 0) &&
+          "You did not add any item to the cart."}
       </p>
     </div>
   );
-}
+};
+
+export default Cart;
