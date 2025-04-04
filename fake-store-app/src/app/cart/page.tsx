1e5702b86c1e5308098bc42700524d0f20be907e
Akkuro <157906714+Akkuro@users.noreply.github.com>
2025-04-04 11:58:40 +0200

    Fix add items to cart

--- fake-store-app/src/app/cart/page.tsx
+++ fake-store-app/src/app/cart/page.tsx
@@ -1,18 +1,37 @@
-import { Product } from "@/interfaces/Product";
+"use client";
+import { useCart } from "@/context/CartContext";
 import React from "react";
 
-interface Props {
-  products: Product[];
-}
+const Cart: React.FC<undefined> = () => {
+  const { cart } = useCart();
 
-const Cart: React.FC<Props> = ({ products }) => {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-4">Your cart</h1>
       <p className="text-lg">
-        {(products == null || products.length === 0) &&
+        {(cart == null || cart.length === 0) &&
           "You did not add any item to the cart."}
       </p>
+      <ul className="space-y-4">
+        {cart.map((item) => (
+          <li
+            key={item.product.id}
+            className="flex justify-between items-center"
+          >
+            <div className="flex items-center space-x-4">
+              <img
+                src={item.product.image}
+                alt={item.product.title}
+                className="w-16 h-16"
+              />
+              <div>
+                <h2 className="font-bold">{item.product.title}</h2>
+                <p>{item.product.price} €</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
