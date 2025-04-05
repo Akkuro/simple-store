c0b2810fd0dd7147a1e4318fb074039701d3da33
Akkuro <157906714+Akkuro@users.noreply.github.com>
2025-04-05 23:03:29 +0200

    Add sticky navbar

--- fake-store-app/src/components/Navbar.tsx
+++ fake-store-app/src/components/Navbar.tsx
@@ -3,18 +3,18 @@ import { AiOutlineLogin, AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-gray-800 text-white p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <span className="text-xl font-bold cursor-pointer">Fake Store</span>
         </Link>
         <div className="flex gap-6 justify-between">
-            <Link href="/cart">
-              <AiOutlineShoppingCart className="text-2xl cursor-pointer" />
-            </Link>
-            <Link href="/login">
-              <AiOutlineLogin className="text-2xl cursor-pointer" />
-            </Link>
+          <Link href="/cart">
+            <AiOutlineShoppingCart className="text-2xl cursor-pointer" />
+          </Link>
+          <Link href="/login">
+            <AiOutlineLogin className="text-2xl cursor-pointer" />
+          </Link>
         </div>
       </div>
     </nav>
